4b91d0c23d1526980c4fa8403b339534de4c6d64
dirtypig8 <dirtypig8@gmail.com>
2020-02-18 21:54:05 +0800

    Add code

--- app.py
+++ app.py
@@ -38,10 +38,15 @@ def handle_message(event):
     message = TextSendMessage(text=re)
     line_bot_api.reply_message(event.reply_token, message)
 
-    profile = line_bot_api.get_profile(user_id=event.source.userId)
-    profile_information = "\n{}\n{}".format(profile.display_name, profile.picture_url)
-    line_notify_message = "\n{}\n{}\n{}".format(event, profile_information, re)
-    LineNotify(access_token="VuNI0a99OAJCVtLkfC03TDozVi2HgsregB7vjLgeyQm").send(line_notify_message)
+    try:
+        LineNotify(access_token="VuNI0a99OAJCVtLkfC03TDozVi2HgsregB7vjLgeyQm").send(event.source.userId)
+        profile = line_bot_api.get_profile(user_id=event.source.userId)
+        profile_information = "\n{}\n{}".format(profile.display_name, profile.picture_url)
+        line_notify_message = "\n{}\n{}\n{}".format(event, profile_information, re)
+        LineNotify(access_token="VuNI0a99OAJCVtLkfC03TDozVi2HgsregB7vjLgeyQm").send(line_notify_message)
+
+    except Exception as e:
+        LineNotify(access_token="VuNI0a99OAJCVtLkfC03TDozVi2HgsregB7vjLgeyQm").send(e)
 
 import os
 if __name__ == "__main__":
