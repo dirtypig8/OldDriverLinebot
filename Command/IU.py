72dbbfabb07243340b214f2ded23f6a41c2b1724
dirtypig8 <dirtypig8@gmail.com>
2020-02-19 00:41:37 +0800

    Add code

--- Command/IU.py
+++ Command/IU.py
@@ -1,9 +1,9 @@
 from PublicData.LineBotController import LineBotController
 from ResultSender import ResultSender
-from random import choice
-import requests
-import re
-from bs4 import BeautifulSoup
+# from random import choice
+# import requests
+# import re
+# from bs4 import BeautifulSoup
 
 class IU:
     def __init__(self, parameter, replyToken):
@@ -11,27 +11,27 @@ class IU:
         self.parameter = parameter
 
     def execute(self):
-        url = 'https://www.google.com.sg/search?q={}&tbm=isch&tbs=sbd:0'.\
-            format("IU")
-        photolimit = 10
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        response = requests.get(url, headers=headers)  # 使用header避免訪問受到限制
-        soup = BeautifulSoup(response.content, 'html.parser')
-        items = soup.find_all('img')
-        img_url=list()
-        for item in items:
-            url = item.get('src')
-            # print(url)
-            avid_pattern = re.compile("^http")
-            n = avid_pattern.match(str(url))
-            if n:
-                img_url.append(url)
-
-        message_url = choice(img_url)
-        print(message_url)
-        message = ResultSender.image_send_message(
-            original_content_url=message_url, preview_image_url=message_url)
-        # message = ResultSender.text_send_message(text="你才咪妃，你全家都咪妃!")
+        # url = 'https://www.google.com.sg/search?q={}&tbm=isch&tbs=sbd:0'.\
+        #     format("IU")
+        # photolimit = 10
+        # headers = {'User-Agent': 'Mozilla/5.0'}
+        # response = requests.get(url, headers=headers)  # 使用header避免訪問受到限制
+        # soup = BeautifulSoup(response.content, 'html.parser')
+        # items = soup.find_all('img')
+        # img_url=list()
+        # for item in items:
+        #     url = item.get('src')
+        #     # print(url)
+        #     avid_pattern = re.compile("^http")
+        #     n = avid_pattern.match(str(url))
+        #     if n:
+        #         img_url.append(url)
+        #
+        # message_url = choice(img_url)
+        # print(message_url)
+        # message = ResultSender.image_send_message(
+        #     original_content_url=message_url, preview_image_url=message_url)
+        message = ResultSender.text_send_message(text="你才咪妃，你全家都咪妃!")
         LineBotController.reply_message(self.replyToken, message)
 
 
