0b3423327145b0b0a5f094dcf2c4bb9df2df4813
SeaDer <d1014181006@gm.lhu.edu.tw>
2020-02-19 18:40:01 +0800

    add code

--- Command/Error.py
+++ Command/Error.py
@@ -5,5 +5,6 @@ class NotFoundCommand:
     def __init__(self, replyToken):
         self.replyToken = replyToken
     def execute(self):
-        message = ResultSender.text_send_message(text="感謝您的幫忙")
+        message = ResultSender.text_send_message(text="你要這指令，你要先講!")
+        LineBotController.reply_message(self.replyToken, message)
         LineBotController.reply_message(self.replyToken, message)
