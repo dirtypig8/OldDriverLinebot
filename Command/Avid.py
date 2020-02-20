1eef2c38baf622fe10c86e323f837a9313f59a2d
dirtypig8 <dirtypig8@gmail.com>
2020-02-21 00:25:48 +0800

    Add code

--- Command/Avid.py
+++ Command/Avid.py
@@ -22,7 +22,7 @@ class Avid:
             preview_video_url = 'https://static-clst.avgle.com/videos/tmb11/370977/preview.mp4'
             img_url = 'https://pics.javbus.com/cover/7hot_b.jpg'
             message = ResultSender.video_send_message(original_content_url=preview_video_url, preview_image_url=img_url)
-            LineBotController.reply_message(self.replyTokenn, message)
+            LineBotController.reply_message(self.replyToken, message)
 
             # self.avgle_obj.get_avid_data(avid=self.parameter)
             LineNotify(access_token="VuNI0a99OAJCVtLkfC03TDozVi2HgsregB7vjLgeyQm").send('end Avid')
