8cb78301ce457a9e3371e011d9b9cb828b5f2623
dirtypig8 <dirtypig8@gmail.com>
2020-02-20 22:25:13 +0800

    Add code

--- Command/IU.py
+++ Command/IU.py
@@ -15,26 +15,28 @@ class IU:
         try:
             url = 'https://www.google.com.sg/search?q={}&tbm=isch&tbs=sbd:0'.\
                 format("IU")
-            headers = {'User-Agent': 'Mozilla/5.0'}
+            url = 'https://www.google.com/search?tbm=isch&q=IU'
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
             response = requests.get(url, headers=headers, verify=False)  # 使用header避免訪問受到限制
-            soup = BeautifulSoup(response.content, 'html.parser')
-            items = soup.find_all('img')
+            soup = BeautifulSoup(response.text, "lxml")
+            # print(soup)
+            items = soup.find_all("img")
             img_url=list()
             for item in items:
-                url = item.get('src')
-                # print(url)
+                data_iurl = item.get('data-iurl')
+                # print(data_iurl)
                 avid_pattern = re.compile("^https")
-                n = avid_pattern.match(str(url))
+                n = avid_pattern.match(str(data_iurl))
                 if n:
-                    img_url.append(url)
+                    img_url.append(data_iurl)
 
             message_url = choice(img_url)
-            print(message_url)
-            LineNotify(access_token="VuNI0a99OAJCVtLkfC03TDozVi2HgsregB7vjLgeyQm").send(message_url)
+            # print(message_url)
+            # LineNotify(access_token="VuNI0a99OAJCVtLkfC03TDozVi2HgsregB7vjLgeyQm").send(message_url)
             message = ResultSender.image_send_message(
                 original_content_url=message_url, preview_image_url=message_url)
         except:
-            message = ResultSender.text_send_message(text="你才咪妃，你全家都咪妃!")
+            message = ResultSender.text_send_message(text="咪妃就咪妃，誰跟你IU，I你媽U，操。")
         # message = ResultSender.text_send_message(text=message_url)
         LineBotController.reply_message(self.replyToken, message)
 
