14efb15d0c56de36eb839a666a1686445c008966
dirtypig8 <dirtypig8@gmail.com>
2020-02-21 00:19:39 +0800

    Add code

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def callback():
 def handle_message(event):
     # message = TextSendMessage(text=event.message.text)
     # re = "你輸入的是: '{}'\n\n感謝您的幫忙".format(event.message.text)
-    preview_video_url = 'https://shareboxnow.com/wp-content/uploads/2020/02/IMG_0469.mp4'
+    preview_video_url = 'https://static-clst.avgle.com/videos/tmb11/370977/preview.mp4'
     img_url = 'https://pics.javbus.com/cover/7hot_b.jpg'
     message = VideoSendMessage(original_content_url=preview_video_url, preview_image_url=img_url)
     # message = TextSendMessage(text=re)
