80e2f4984152b7719f97b39b83edc5c2cef02271
dirtypig8 <dirtypig8@gmail.com>
2020-02-24 23:53:09 +0800

    Add code

--- Command/Fuzzy.py
+++ Command/Fuzzy.py
@@ -23,11 +23,11 @@ class Fuzzy:
 
             message_list.append(ResultSender.text_send_message(text=result))
             LineBotController.reply_message(self.replyToken, message_list)
+            LineBotController.reply_message(self.replyToken, message_list)
         except Exception as e:
             error_message = '比對失敗 : {}'.format(e)
             message_list.append(ResultSender.text_send_message(text=error_message))
-
-        LineBotController.reply_message(self.replyToken, message_list)
+            LineBotController.reply_message(self.replyToken, message_list)
 
 if __name__ == "__main__":
     Fuzzy(parameter=['abc1111', '1111'], replyToken='cf19f91ee96f43d79bb3fc362eba8be5').execute()
